10e639d149b65172e2651cdfdca7ad70cbfa2384
Naveed Ahmed <naveedahmedgopang76@gmail.com>
2026-02-14 05:30:57 +0500

    middleware limit set

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,18 @@
-export { auth as middleware } from "@/auth";
+// export { auth as middleware } from "@/auth";
+
+// export const config = {
+//   matcher: ["/dashboard/:path*", "/jobs/post"],
+// };
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get("next-auth.session-token"); // or your cookie name
+  if (!token) {
+    return NextResponse.redirect(new URL("/auth/signin", request.url));
+  }
+  return NextResponse.next();
+}
 
 export const config = {
   matcher: ["/dashboard/:path*", "/jobs/post"],
